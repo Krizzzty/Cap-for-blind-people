87ed610db7a3d567309b52c146ea4517cfcf80e8
Kristiena <kris.benn18@gmail.com>
2023-03-22 23:40:31 +0530

    first attempt

---
+++ Object_Detection_Files/main.py
@@ -0,0 +1,56 @@
+import cv2
+import pyttsx3
+import threading
+
+thres = 0.45 # Threshold to detect object
+# Initialize the text-to-speech engine
+engine = pyttsx3.init()
+
+# Set properties for the voice
+engine.setProperty('rate', 150)  # Speed of the voice
+engine.setProperty('volume', 0.8)  # Volume of the voice
+
+# Function to run speech output in a separate thread
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+cap = cv2.VideoCapture('WhatsApp.mp4')
+#address='http://10.10.173.157:8080/video'
+#cap.open(address)
+cap.set(3,1280)
+cap.set(4,720)
+cap.set(10,70)
+
+classNames= []
+classFile = 'coco.names'
+with open(classFile,'rt') as f:
+    classNames = f.read().rstrip('\n').split('\n')
+
+configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
+weightsPath = 'frozen_inference_graph.pb'
+
+net = cv2.dnn_DetectionModel(weightsPath,configPath)
+net.setInputSize(240,240)
+net.setInputScale(1.0/ 127.5)
+net.setInputMean((127.5, 127.5, 127.5))
+net.setInputSwapRB(True)
+
+while True:
+    success,img = cap.read()
+    classIds, confs, bbox = net.detect(img,confThreshold=thres)
+    print(classIds,bbox)
+
+    if len(classIds) != 0:
+        for classId, confidence,box in zip(classIds.flatten(),confs.flatten(),bbox):
+            cv2.rectangle(img,box,color=(0,255,0),thickness=2)
+            cv2.putText(img,classNames[classId-1].upper(),(box[0]+10,box[1]+30),
+            cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+            cv2.putText(img,str(round(confidence*100,2)),(box[0]+200,box[1]+30),
+            cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+            text = classNames[classId-1]+' detected'
+            # Start a new thread to run speech output
+            threading.Thread(target=speak, args=(text,)).start()
+
+    cv2.imshow("Output",img)
+    cv2.waitKey(1)
